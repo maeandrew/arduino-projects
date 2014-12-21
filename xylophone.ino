52bc045c1a2eef6340ae9961588f42db0dc86299
Andrew Mae <amey.pro@gmail.com>
2014-12-21 19:59:41 +0200

    new version with many melodies

--- xylophone.ino
+++ xylophone.ino
@@ -23,16 +23,65 @@ Servo myServo;
 
 int curent_step = 0;   
 
+
 //calibration
-//int length = 16;
-//char notes[] = "cdefgabCCbagfedc";
-//int beats[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+// Length must equal the total number of notes and spaces
+int length0 = 16;
+
+// Notes is an array of text characters corresponding to the notes
+// in your song. A space represents a rest (no tone)
+char notes0[] = "cdefgabCCbagfedc";
+
+// Beats is an array of values for each note and rest.
+// A "1" represents a quarter-note, 2 a half-note, etc.
+// Don't forget that the rests (spaces) need a length as well.
+int beats0[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 //jingle_bells
-int length = 26;
-char notes[] = "eeeeeeegcde fffffeeeeddedg";
-int beats[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
+int length1 = 26;
+char notes1[] = "eeeeeeegcde fffffeeeeddedg";
+int beats1[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
 
+int length2 = 15; // the number of notes
+char notes2[] = "ccggaagffeeddc "; // a space represents a rest
+int beats2[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+
+int length3 = 25; // the number of windy notes
+char notes3[] = "gagfefgdefefggagfefgdgec "; // a space represents a rest
+int beats3[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 4 };
+
+int length4 = 32; // the number of rainy notes
+char notes4[] = "geggegeggeggeaggegfedcdegfedcde ";
+int beats4[] = { 2, 2, 1, 1, 2, 2, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+
+int length5 = 27; // the number of sunny notes
+char notes5[] = "cccegecdddbagcccegecddgabbcc ";
+int beats5[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 2, 4 };
+
+
+int length6 = 33; // the number of cloudy notes
+char notes6[] = "cdeccdecefgefggagfecgagfecdgcdgc "; // a space represents a rest
+int beats6[] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 2, 2, 4, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 4, 2, 2, 4, 4 };
+
+// solfeo Maria
+int length7 = 29; // the number of notes
+char notes7[] = "cccdefefgfedec cccdefefgfedc "; // a space represents a rest
+int beats7[] = { 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1,  5 };
+
+// solfeo estrella
+int length8 = 50; // the number of notes
+char notes8[] = "cccdefefgfedec cccdefefgfedc efgagfedec efgagfedc "; // a space represents a rest
+int beats8[] = { 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1,  3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5 };
+
+
+int length9 = 18;
+char notes9[] = "cdfda ag cdfdg gf ";
+int beats9[] = {1,1,1,1,1,1,4,4,2,1,1,1,1,1,1,4,4,2};
+
+int allLength[10] = {length1,length2,length3,length4,length5,length6,length7,length8,length9};
+char *allNotes[10] = {notes1,notes2,notes3,notes4,notes5,notes6,notes7,notes8,notes9};
+int *allBeats[10] = {beats1,beats2,beats3,beats4,beats5,beats6,beats7,beats8,beats9};
 
 int tempo = 100;
 int stepper_speed = 40;
@@ -43,7 +92,7 @@ void setup() {
   
   myServo.attach(2);
   
-  intro();
+//  intro();
    
 }
 
@@ -72,16 +121,22 @@ void intro() {
 }
 
 void loop() {
-    
-   for (int i = 0; i < length; i++) {
-      if (notes[i] == ' ') {
-        delay(beats[i] * tempo); // rest
-      } else {
-        playNote(notes[i], beats[i] * tempo);
-      }
-      
-      // pause between notes
-      delay(tempo / 2); 
+ 
+     
+   for (int m = 0; m < sizeof(allLength); m++) {
+     for (int i = 0; i < allLength[m]; i++) {
+        if (allNotes[m][i] == ' ') {
+          delay(allBeats[m][i] * tempo); // rest
+        } else {
+          playNote(allNotes[m][i], allBeats[m][i] * tempo);
+        }
+         Serial.print(m);
+         Serial.print(' ');
+         Serial.println(i);
+        // pause between notes
+        delay(tempo / 2); 
+    }
+    delay(2000);
   }
   
   delay(5000);
@@ -140,16 +195,13 @@ void playTone(int tone, int duration){
     default: 
     break;
   }
-  myStepper.step(step);  
-  kick();  
-  
-  move_duration = calcTimeForMoving(old_step, curent_step);
-  
-  Serial.print(duration);
-  Serial.print(' ');
-  Serial.println(move_duration);
-  
+     
+  move_duration = calcTimeForMoving(old_step, curent_step);    
   delay(duration);
+  
+  myStepper.step(step);  
+  kick(); 
+  
 }
 
 int calcTimeForMoving(int curent_step, int step){    
